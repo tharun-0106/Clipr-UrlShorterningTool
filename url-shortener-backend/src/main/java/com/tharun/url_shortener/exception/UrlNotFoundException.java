fe5684c0bbf4785ce3a12189ba4498f22d8ef8ef
Tharun <imtharun01@gmail.com>
2026-05-25 13:07:26 +0530

    feat: Adding UI

--- url-shortener-backend/src/main/java/com/tharun/url_shortener/exception/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tharun.url_shortener.exception;
-
-public class UrlNotFoundException extends RuntimeException{
-    public UrlNotFoundException(String message){
-        super(message);
-    }
-}
